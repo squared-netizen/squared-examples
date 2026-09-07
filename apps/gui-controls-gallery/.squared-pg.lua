a25c601b88e2951e9e0c06e9e6825e21e2d68c4d
squared-netizen <jesse.rosesquared@gmail.com>
2026-09-06 20:04:16 -0700

    See README.md

--- apps/gui-controls-gallery/.squared-pg.lua
+++
@@ -1,9 +0,0 @@
-return {
-    format = 3,
-    generator = "squared-pg",
-    generator_version = "0.6.0-dev.6",
-    name = "squared-gui-controls-gallery",
-    package = "dev.squarednetizen.guicontrolsgallery",
-    version = "0.1.0",
-    template = "dev.squarednetizen.template.android-sdl2-lua@0.6.0-dev.16"
-}
--- apps/gui-controls-gallery/app/src/main/assets/lua/plugins.lua
+++
@@ -1,9 +0,0 @@
---- Deterministic plug-in registry.
---
--- Plug-ins are loaded and dispatched in this exact order.
--- @script plugins
-
-return {
-    "diagnostics",
-    "orbit"
-}
